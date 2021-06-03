9723c8759833a14c9dd282a877b76985270588c6
Nadejda <mezz@74.ru>
2021-06-03 15:50:38 +0500

    URL считывание страницы сайта html-код.

---
+++ src/com/examle/app/Main.java
@@ -0,0 +1,29 @@
+package com.examle.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.baeldung.com/");
+        URLConnection con = url.openConnection();
+        try (
+            Reader reader = new InputStreamReader(
+                    con.getInputStream(), "utf-8");
+            BufferedReader buf = new BufferedReader(reader);
+            )
+
+            {
+                String line = "";
+                while ((line = buf.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            }
+
+    }
+}
