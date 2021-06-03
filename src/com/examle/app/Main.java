58f68a47b7b70981057db16755bf70be9de74ff1
Nadejda <mezz@74.ru>
2021-06-03 16:10:07 +0500

    http URL считывание страницы сайта html-код.

--- src/com/examle/app/Main.java
+++ src/com/examle/app/Main.java
@@ -3,6 +3,7 @@ package com.examle.app;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -10,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         URL url = new URL("https://www.baeldung.com/");
-        URLConnection con = url.openConnection();
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
         try (
             Reader reader = new InputStreamReader(
                     con.getInputStream(), "utf-8");
@@ -24,6 +25,6 @@ public class Main {
                 }
 
             }
-
+        System.out.println(con.getReadTimeout());
     }
 }
